6ff830fb74cae2f7ea221cf183a1686c0bb3d78b
Arif Meman <arifmeman786@gmail.com>
2025-03-02 04:24:00 +0000

    Day1

--- Day1.tf
+++ Day1.tf
@@ -1,1 +1,7 @@
 ############### Day 1 - Terraform Basics ###################
+# Terraform Basic Commands
+# terraform init
+# terraform validate
+# terraform plan
+# terraform apply
+# terraform destroy
